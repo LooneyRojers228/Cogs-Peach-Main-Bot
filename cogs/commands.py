74d462602bd19ee8be78bb0d24596b595bf133c7
LooneyRojers228 <80281077+LooneyRojers228@users.noreply.github.com>
2021-03-29 13:29:46 +0300

    Update commands.py

--- cogs/commands.py
+++ cogs/commands.py
@@ -5,7 +5,7 @@ import random
 from discord.ext import commands
 from typing import Optional
 from discord import Embed, Member
-
+from datetime import datetime
 
 
 
@@ -47,15 +47,15 @@ class User(commands.Cog):
 	@commands.command(aliases = ["инфо о серваке"])
 	async def server_info(self, ctx):
 		await ctx.message.delete()
-		emb = discord.Embed (title = 'Информация о сервере :clipboard:', colour=ctx.guild.owner.colour, timestamp=datetime.utcnow())
-		emb.set_thumbnail(url=ctx.guild.icon_url)
-		emb.add_field(name ='ID', value = ctx.guild.id)
-		emb.add_field(name ='Владелец', value = ctx.guild.owner)
-		emb.add_field(name ='Регион', value = ctx.guild.region)
-		emb.add_field(name ='Дата создания сервера', value = ctx.guild.created_at.strftime("%d/%m/%Y %H:%M:%S"))
-		emb.add_field(name ='Участников', value = ctx.guild.members)
-		emb.add_field(name ='Роли', value = ctx.guild.roles)
-		emb.set_footer (text ='Peach Bot Main', icon_url=ctx.bot.user.avatar_url)
+		emb = discord.Embed (title = 'Информация о сервере :clipboard:', colour=discord.Color.gold(), timestamp=datetime.utcnow())
+		# emb.set_thumbnail(url=ctx.guild.icon_url)
+		# emb.add_field(name ='ID', value = ctx.guild.id)
+		# emb.add_field(name ='Владелец', value = ctx.guild.owner)
+		# emb.add_field(name ='Регион', value = ctx.guild.region)
+		# emb.add_field(name ='Дата создания сервера', value = ctx.guild.created_at.strftime("%d/%m/%Y %H:%M:%S"))
+		# emb.add_field(name ='Участников', value = ctx.guild.members)
+		# emb.add_field(name ='Роли', value = ctx.guild.roles)
+		# emb.set_footer (text ='Peach Bot Main', icon_url=ctx.bot.user.avatar_url)
 		await ctx.send (embed = emb, delete_after=30)
 
 
