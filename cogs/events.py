b52df1f30f1a2e20e766bf49b699a335eb24df3c
LooneyRojers228 <80281077+LooneyRojers228@users.noreply.github.com>
2021-05-14 17:46:11 +0300

    Update events.py

--- cogs/events.py
+++ cogs/events.py
@@ -17,6 +17,29 @@ class Event(commands.Cog):
 		await self.client.change_presence (status = discord.Status.online, activity = discord.Game('!tiktok | !help'))
 
 
+	async def on_raw_reaction_add(self, payload):
+		if payload.message_id == config.POST_ID:
+			channel = self.get_channel(payload.channel_id) 
+			message = await channel.fetch_message(payload.message_id) 
+			member = utils.get(message.guild.members, id=payload.user_id) 
+
+			try:
+				emoji = str(payload.emoji) 
+				role = utils.get(message.guild.roles, id=config.ROLE[emoji],) 
+			
+				
+				await member.add_roles(role)
+					
+
+				print('[SUCCESS] Пользователь {0.display_name} получил роль мероприятия {1.name}'.format(member, role))
+
+				user = await Client.fetch_user(user_id=member)
+				await user.send('test')
+
+
+
+
+
 	@commands.Cog.listener()
 	async def on_command_error(self, ctx, error):
 		pass
